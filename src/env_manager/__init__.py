ea81def0d0d98b5f055a62cfcc8cf5a4f01db85d
bastianibanez <bastian.miba@gmail.com>
2026-03-31 19:21:51 -0300

    feat(02-01): add DecryptionError/DecryptionIssue and eciespy optional dep

--- src/env_manager/__init__.py
+++ src/env_manager/__init__.py
@@ -3,10 +3,20 @@
 from .manager import ConfigManager, get_config, init_config, require_config
 from .base import SecretLoader
 from .environment import EnvironmentConfig
+from .exceptions import (
+    ConfigValidationError,
+    ConfigValidationIssue,
+    DecryptionError,
+    DecryptionIssue,
+)
 from .factory import create_loader
 
 __all__ = [
     "ConfigManager",
+    "ConfigValidationError",
+    "ConfigValidationIssue",
+    "DecryptionError",
+    "DecryptionIssue",
     "EnvironmentConfig",
     "get_config",
     "init_config",
